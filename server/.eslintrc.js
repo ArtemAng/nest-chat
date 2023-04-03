884c32ae11ff2f08a2f06bbee74c809dcd3ec7e5
ArtemAng <angalevichartem@gmail.com>
2023-04-03 14:16:21 +0300

    build: docker config

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,25 +1,214 @@
+const path = require('path');
+
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
     tsconfigRootDir: __dirname,
-    sourceType: 'module',
+    sourceType: 'module'
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  overrides: [
+    {
+      files: ['**/*/*.dto.ts'],
+      rules: {
+        'max-classes-per-file': 'off'
+      }
+    }
+  ],
+  plugins: [
+    '@typescript-eslint/eslint-plugin',
+    'import',
+    'promise',
+    'unicorn',
+    'prettier'
+  ],
   extends: [
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
+    'plugin:eslint-comments/recommended',
+    'airbnb-base',
+    'airbnb-typescript/base',
+    'plugin:unicorn/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:prettier/recommended'
   ],
   root: true,
   env: {
     node: true,
-    jest: true,
+    jest: true
   },
-  ignorePatterns: ['.eslintrc.js'],
+  ignorePatterns: ['.eslintrc.js', 'commitlint.config.js'],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/consistent-type-assertions': 'warn',
+    '@typescript-eslint/consistent-type-imports': [
+      'error',
+      {
+        disallowTypeAnnotations: true,
+        fixStyle: 'inline-type-imports',
+        prefer: 'type-imports'
+      }
+    ],
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/no-array-constructor': 'warn',
+    '@typescript-eslint/no-duplicate-imports': 'off',
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-misused-promises': [
+      'error',
+      {
+        checksVoidReturn: {
+          arguments: false,
+          attributes: false
+        }
+      }
+    ],
+    '@typescript-eslint/no-redeclare': 'error',
+    '@typescript-eslint/no-unnecessary-condition': 'error',
+    '@typescript-eslint/no-unsafe-argument': 'warn',
+    '@typescript-eslint/no-unsafe-assignment': 'off',
+    '@typescript-eslint/no-unused-expressions': 'error',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { ignoreRestSiblings: true }
+    ],
+    '@typescript-eslint/no-use-before-define': [
+      'error',
+      {
+        functions: false,
+        classes: false,
+        variables: false,
+        typedefs: false
+      }
+    ],
+    '@typescript-eslint/no-useless-constructor': 'error',
+    '@typescript-eslint/padding-line-between-statements': [
+      'error',
+      {
+        blankLine: 'always',
+        prev: '*',
+        next: ['interface', 'type']
+      },
+      {
+        blankLine: 'always',
+        prev: '*',
+        next: ['return']
+      },
+      {
+        blankLine: 'always',
+        prev: 'block-like',
+        next: '*'
+      },
+      {
+        blankLine: 'always',
+        prev: 'directive',
+        next: '*'
+      },
+      { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
+      {
+        blankLine: 'any',
+        prev: ['const', 'let', 'var'],
+        next: ['const', 'let', 'var']
+      }
+    ],
+    'class-methods-use-this': 'off',
+    'default-case': 'off',
+    'eslint-comments/no-unused-disable': 'error',
+    'eslint-comments/require-description': [
+      'error',
+      { ignore: ['eslint-enable'] }
+    ],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never'
+      }
+    ],
+    'import/no-default-export': 'error',
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: [
+          '**/*.test.{js,jsx,ts,tsx}',
+          '**/*.spec.{js,jsx,ts,tsx}',
+          '**/*.e2e-spec.{js,jsx,ts,tsx}'
+        ]
+      }
+    ],
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'always',
+        pathGroupsExcludedImportTypes: ['react'],
+        groups: [
+          'unknown',
+          'builtin',
+          'external',
+          'internal',
+          ['parent', 'sibling', 'index']
+        ],
+        pathGroups: [
+          {
+            pattern: 'react',
+            group: 'builtin',
+            position: 'before'
+          }
+        ],
+        alphabetize: { order: 'asc', caseInsensitive: true }
+      }
+    ],
+    'import/prefer-default-export': 'off',
+    'no-array-constructor': 'off',
+    'no-await-in-loop': 'off',
+    'no-bitwise': 'off',
+    'no-console': ['error', { allow: ['warn', 'error', 'debug'] }],
+    'no-dupe-class-members': 'off',
+    'no-redeclare': 'off',
+    'no-restricted-syntax': 'off',
+    'no-shadow': 'off',
+    'no-undef': 'off',
+    'no-underscore-dangle': 'off',
+    'no-unused-expressions': 'off',
+    'no-unused-vars': 'off',
+    'no-use-before-define': 'off',
+    'no-useless-constructor': 'off',
+    'padding-line-between-statements': 'off',
+    'promise/always-return': 'off',
+    'promise/avoid-new': 'off',
+    'promise/catch-or-return': 'off',
+    'promise/no-callback-in-promise': 'warn',
+    'promise/no-native': 'off',
+    'promise/no-nesting': 'warn',
+    'promise/no-promise-in-callback': 'warn',
+    'promise/no-return-in-finally': 'warn',
+    'promise/no-return-wrap': 'error',
+    'promise/param-names': 'error',
+    'unicorn/no-array-callback-reference': 'off',
+    'unicorn/no-array-reduce': 'off',
+    'unicorn/no-await-expression-member': 'off',
+    'unicorn/no-null': 'off',
+    'unicorn/prefer-module': 'off',
+    'unicorn/prefer-top-level-await': 'off',
+    'unicorn/prevent-abbreviations': [
+      'error',
+      {
+        allowList: {
+          ProcessEnv: true,
+          e2e: true
+        },
+        checkFilenames: false
+      }
+    ]
   },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+        project: [path.join(process.cwd(), './tsconfig.json')]
+      }
+    }
+  }
 };
