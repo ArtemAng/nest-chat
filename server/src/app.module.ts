73a10e94265c6e7b364df108ff7979066ed1758e
ArtemAng <angalevichartem@gmail.com>
2023-04-03 15:46:04 +0300

    feat: database config

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from './app.service';
+import { ormConfig } from './configs/orm.config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    TypeOrmModule.forRoot({
+      ...ormConfig,
+      logging: ['warn', 'error', 'migration'],
+      logger: 'simple-console',
+      autoLoadEntities: true,
+      synchronize: false
+    })],
+  controllers: [],
   providers: [AppService],
 })
 export class AppModule {}
